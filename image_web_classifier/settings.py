75eb7c7b29ee6444698eb0df37d3d332d42deb94
alenaporoskun <alenka200124@gmail.com>
2024-08-29 14:29:20 +0300

    changes in settings2

--- image_web_classifier/settings.py
+++ image_web_classifier/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['classifyme.onrender.com']
+ALLOWED_HOSTS = ['classifyme.onrender.com', '127.0.0.1']
 
 
 # Application definition
